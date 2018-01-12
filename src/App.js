ca5ac5763420073fd5cba0c1aacd7fd6a7e66967
Nonkavit <kavit5242@gmail.com>
2018-01-12 16:25:27 +0700

    new

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 import _ from 'lodash'
 
 class App extends Component {
+  SYMBOL = ['OMG','BTC','XRP']
   constructor(props){
     super(props)
     this.state = {}
@@ -13,8 +14,13 @@ class App extends Component {
     let self = this
     axios.get('https://api.coinmarketcap.com/v1/ticker/?convert=THB')
     .then(function (response) {
-      let btc = _.find(response.data, ['symbol', 'BTC'])
-      let omg = _.find(response.data, ['symbol', 'OMG'])
+
+     _.each(self.SYMBOL,symbol => {
+
+      _.find(response.data, ['symbol', symbol]).price_thb
+
+     }) 
+        
      
 
       self.setState({
@@ -32,9 +38,10 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Ticker symbol="BTC" price={this.state.BTC}/>
-        <Ticker symbol="OMG" price={this.state.OMG}/>
-      </div>
+       { 
+        _.map(this.SYMBOLS,symbol) => <Ticker symbol={symbol} price={this.state.[symbol]/>
+       }
+        </div>
     );
   }
 }
